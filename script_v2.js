b8dca17c342161acdd4bc9e589bc5c2a6a11347e
kennyhouston-hw <74400190+kennyhouston-hw@users.noreply.github.com>
2025-12-07 18:30:28 +0300

    Update script_v2.js

--- script_v2.js
+++ script_v2.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const tocContainer = document.createElement('div');
     tocContainer.id = 'table-of-contents';
     tocContainer.setAttribute('aria-labelledby', 'toc-main-header');
-    tocContainer.innerHTML = `<span id="toc-main-header" class="toc-header">В этой статье:</span>`;
+    tocContainer.innerHTML = `<span id="toc-main-header" class="toc-header">Содержание:</span>`;
     const tocList = document.createElement('ul');
     tocContainer.appendChild(tocList);
     headers.forEach((header, index) => {
